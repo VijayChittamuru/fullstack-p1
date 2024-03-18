40e3856e7452b943f6951284f14b07cb14181227
Vijay Chittamuru <vvc4@case.edu>
2024-03-18 16:43:17 -0400

    Latest updates

--- FullStack codepen P1.js
+++ FullStack codepen P1.js
@@ -1,44 +1,66 @@
 $(document).ready(function(){
-    $('#resetButton').click(function() {
-      toastr.info('Form has been reset.');
-      $(this).blur();
-    });
-  
-    $('#submitButton').click(submitActions());
+  // On click action for the reset button
+  $('#resetButton').click(() => {
+    toastr.info('Form has been reset.');
+    $(this).blur();
   });
 
-function submitActions() {
-  var valid = true;
-  const details = [$("#username"), 
-                   $("#firstName"),
-                   $("#lastName"),
-                   $("#phone"),
-                   $("#fax"),
-                   $("#email")];
-  details.forEach(field => {
-    if(field.val() == "") {
-      field.addClass('has-error');
-      valid = false;
-    }
-    else {
-      field.removeClass('has-error');
-    }
+  // On click action for the submit button
+  $('#submitButton').click(() => {
+    submitActions();
+    $(this).blur();
   });
-  
-  if(!valid) {
-    toastr.error("Missing info. Please check all fields");
-    return;
+
+  // Listens for changes in schedule components
+  $('#adults, #checkin, #checkout').change(scheduleChange);
+});
+
+  function scheduleChange(){
+    var adults = $('#adults').val();
+    var checkIn = $('#checkin').val();
+    var checkOut = $('#checkout').val();
+    if (adults && checkIn && checkOut) {
+      var days = moment(checkOut).diff(moment(checkIn), 'days');
+      $('#days').val(days);
+      $('#cost').val(adults * days * 150);
+    }
   }
   
-  var cost = parseInt($("#cost").val());
-  
-  if($("#cost").val() == ""){
-      toastr.error("Cost not calculated!");
+  function submitActions() {
+    event.preventDefault();
+    var valid = true;
+    const details = [$("#username"), 
+                     $("#firstName"),
+                     $("#lastName"),
+                     $("#phone"),
+                     $("#fax"),
+                     $("#email")];
+    details.forEach(field => {
+      if(field.val() == "") {
+        field.closest('.form-group').addClass('has-error');
+        valid = false;
+      }
+      else {
+        field.closest('.form-group').removeClass('has-error');
+      }
+    });
+    
+    if(!valid) {
+      toastr.error("Missing info. Please check marked fields");
       return;
     }
-  
-  if(cost < 0) {
-    toastr.error("Cost is negative, please check the dates.")
-    return;
+
+    if($("#cost").val() == ""){
+      toastr.error("Cost not calculated, please check the dates.");
+      return;
+    }
+
+    var cost = parseInt($("#cost").val());
+
+    if(cost <= 0) {
+      toastr.error("Cost must be positive, please check the dates.");
+      return;
+    }
+
+    toastr.success("Successfully submitted!");
   }
-}
